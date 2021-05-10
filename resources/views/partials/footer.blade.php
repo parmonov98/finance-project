2335128f74141d5e8f8c7da23d0231a136b0fee0
da-ci <ali.silarbi00@gmail.com>
2021-05-10 11:03:32 +0000

    small fix

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="c-footer" >
-    <div><a href="https://coreui.io">Daci</a> © 2020 </div>
+    <div><a>Daci</a> © 2020 </div>
     <div class="ml-auto">Powered by&nbsp;<a href="https://coreui.io/">Ali Silarbi</a></div>
 </footer>
