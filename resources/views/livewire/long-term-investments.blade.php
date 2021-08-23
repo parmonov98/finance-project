df651bedd3eabae12a6c3a255012d890a4df0a8e
Murod <parmonov98@yandex.ru>
2021-08-23 12:49:39 +0500

    % removed from long term investment's return on invest value

--- resources/views/livewire/long-term-investments.blade.php
+++ resources/views/livewire/long-term-investments.blade.php
@@ -112,7 +112,7 @@
                 <tbody>
                     @foreach($datas as $data) <tr>
                         <td>{{ $data->formatDate() }}</td>
-                        <td>{{ $data->return_on_invest*100 }}%</td>
+                        <td>{{ $data->return_on_invest }}%</td>
                         <td>{{ $data->fees }}%</td>
                         <td>$ {{ $data->formatNumber($data->monthly_account_fee) }}</td>
                         <td>{{ $data->inflation}}%</td>
